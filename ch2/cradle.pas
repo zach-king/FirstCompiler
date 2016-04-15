d95dae5e2c01ff806f44468ce536f24704484001
Zachary King <kingzach77@gmail.com>
2016-04-15 14:15:14 -0500

    Ch2 - Added multiplication and division

--- ch2/cradle.pas
+++ ch2/cradle.pas
@@ -85,10 +85,41 @@ begin
   GetChar;
 end;
 
-{ Translate a Term }
+{ Parse and Translate a Math Factor }
+procedure Factor;
+begin
+  EmitLn('MOVE #' + GetNum + ', D0');
+end;
+
+{ Recognize and Translate a Multiply }
+procedure Multiply;
+begin
+  Match('*');
+  Factor;
+  EmitLn('MULS (SP)+, D0');
+end;
+
+{ Recognize and Translate a Divide }
+procedure Divide;
+begin
+  Match('/');
+  Factor;
+  EmitLn('MOVE (SP)+, D1');
+  EmitLn('DIVS D1, D0');
+end;
+
+{ Parse and Translate a Math Term }
 procedure Term;
 begin
-  EmitLn('MOVE ' + GetNum + ', D0')
+  Factor;
+  while Look in ['*', '/'] do begin
+    EmitLn('MOVE D0, -(SP)');
+    case Look of
+      '*': Multiply;
+      '/': Divide;
+    else Expected('Mulop');
+    end;
+  end;
 end;
 
 { Recognize and Translate an Add }
